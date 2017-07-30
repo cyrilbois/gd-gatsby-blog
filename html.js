9c469c94af2e98dc736a59b8fe45584d63e4bec8
jackaljack <jackdebidda@gmail.com>
2017-07-30 10:21:08 +0200

    Update meta description

--- html.js
+++ html.js
@@ -25,6 +25,7 @@ module.exports = React.createClass({
         <head>
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="description" content="Personal blog of a software developer experienced in Python and Javascript. I love &#9825; data and visualizations." />
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1.0 maximum-scale=5.0"

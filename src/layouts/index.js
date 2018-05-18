07b2855521fc2b50862d509b0a02d25ed2823a84
jackdbd <jackdebidda@gmail.com>
2018-05-18 17:19:45 +0200

    Refactor Template as functional component

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,76 +5,71 @@ import { rhythm, scale, options } from '../utils/typography'
 import { siteMetadata } from '../../gatsby-config.js'
 import Footer from '../components/Footer';
 
-class Template extends React.Component {
-  render() {
-    const websiteTitle = siteMetadata.title
-    const { location, children } = this.props
-    let header
-    if (location.pathname === '/') {
-      {/* header on the home page */}
-      header = (
-        <h1
-          style={{
-            ...scale(1),
-            fontFamily: options.headerFontFamily.join(','),
-            marginBottom: rhythm(1.5),
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: 'none',
-              textDecoration: 'none',
-              color: 'inherit',
-            }}
-            to={'/'}
-          >
-            {websiteTitle}
-          </Link>
-        </h1>
-      )
-    } else {
-      {/* header on the any other page (e.g. a blog post page) */}
-      header = (
-        <h3
-          style={{
-            fontFamily: options.headerFontFamily.join(','),
-            marginTop: 0,
-            marginBottom: rhythm(0.5),
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: 'none',
-              textDecoration: 'none',
-              color: 'inherit',
-            }}
-            to={'/'}
-          >
-            {websiteTitle}
-          </Link>
-        </h3>
-      )
-    }
-    return (
-      <Container
+
+const Template = (props) => {
+  const websiteTitle = siteMetadata.title
+  const { location, children } = props
+
+  let header
+  if (location.pathname === '/') {
+    {/* header on the home page */}
+    header = (
+      <h1
         style={{
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+          ...scale(1),
+          fontFamily: options.headerFontFamily.join(','),
+          marginBottom: rhythm(1.5),
+          marginTop: 0,
         }}
       >
-        {header}
-        {children()}
-        <Footer />
-      </Container>
+        <Link
+          style={{
+            boxShadow: 'none',
+            textDecoration: 'none',
+            color: 'inherit',
+          }}
+          to={'/'}
+        >
+          {websiteTitle}
+        </Link>
+      </h1>
+    )
+  } else {
+    {/* header on the any other page (e.g. a blog post page) */}
+    header = (
+      <h3
+        style={{
+          fontFamily: options.headerFontFamily.join(','),
+          marginTop: 0,
+          marginBottom: rhythm(0.5),
+        }}
+      >
+        <Link
+          style={{
+            boxShadow: 'none',
+            textDecoration: 'none',
+            color: 'inherit',
+          }}
+          to={'/'}
+        >
+          {websiteTitle}
+        </Link>
+      </h3>
     )
   }
-}
 
-Template.propTypes = {
-  children: React.PropTypes.function,
-  location: React.PropTypes.object,
-  route: React.PropTypes.object,
+  return (
+    <Container
+      style={{
+        maxWidth: rhythm(24),
+        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+      }}
+    >
+      {header}
+      {children()}
+      <Footer />
+    </Container>
+  )
 }
 
 export default Template
